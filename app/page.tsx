46519573478cb1c7ee407bcb15a77dc172123712
AyBee <aybee076@gmail.com>
2026-04-28 19:55:02 +0000

    errors fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="mt-20 bg-amber-700">
+    <div className="mt-20 bg-amber-700 py-10">
+      home <br />
       home
+      
     </div>
   );
 }
